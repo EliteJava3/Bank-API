84ea795eca73f31b43d2fa502f20be007b8e09dd
senorcl0ud <claudemcalpin@gmail.com>
2018-06-21 00:15:22 -0400

    added Address and Customer domains

---
+++ src/main/java/io/elitejava3/BankAPI/domains/Address.java
@@ -0,0 +1,90 @@
+package io.elitejava3.BankAPI.domains;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+    public String street_number;
+    public String street_name;
+    public String city;
+    public String state;
+    public String zipCode;
+
+
+    public Address(){}
+
+    public Address(Long id, String street_number, String street_name, String city, String state, String zipCode) {
+        this.id = id;
+        this.street_number = street_number;
+        this.street_name = street_name;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet_number() {
+        return street_number;
+    }
+
+    public void setStreet_number(String street_number) {
+        this.street_number = street_number;
+    }
+
+    public String getStreet_name() {
+        return street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street_number='" + street_number + '\'' +
+                ", street_name='" + street_name + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/io/elitejava3/BankAPI/domains/Customer.java
@@ -0,0 +1,79 @@
+package io.elitejava3.BankAPI.domains;
+
+import javax.persistence.*;
+import java.util.Set;
+
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+    public Long accountId;
+    public String first_name;
+    public String last_name;
+    @OneToMany
+    @JoinColumn
+    public Set<Address> addresses;
+
+    public Customer(){}
+
+    public Customer(Long id, Long accountId, String first_name, String last_name, Set<Address> addresses) {
+        this.id = id;
+        this.accountId = accountId;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.addresses = addresses;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", accountId=" + accountId +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", addresses=" + addresses +
+                '}';
+    }
+}
