2a8494a76c629f1aa5ab8cfb2a531299acd5f712
iVesperX <aermias@live.com>
2018-06-21 11:37:39 -0400

    pojo changes

--- src/main/java/io/elitejava3/BankAPI/domains/Customer.java
+++ src/main/java/io/elitejava3/BankAPI/domains/Customer.java
@@ -4,108 +4,36 @@ import javax.persistence.*;
 import java.util.Set;
 @Entity
 public class Customer {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private Account account;
     private String first_name;
     private String last_name;
-//  private Set<Address> addresses;
-    private String street_number;
-    private String street_name;
-    private String city;
-    private String state;
-    private String zipCode;
+    private Set<Address> addresses;
 
     public Customer(){}
 
-    public Customer(Long id, String first_name, String last_name, String street_number, String street_name, String city, String state, String zipCode) {
+    public Customer(Long id, Account account, String first_name, String last_name, Set<Address> addresses) {
         this.id = id;
+        this.account = account;
         this.first_name = first_name;
         this.last_name = last_name;
-        this.street_number = street_number;
-        this.street_name = street_name;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
+        this.addresses = addresses;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Account getAccount() { return account; }
+    public void setAccount(Account account) { this.account = account; }
 
+    public String getFirst_name() { return first_name; }
+    public void setFirst_name(String first_name) { this.first_name = first_name; }
 
-    public String getFirst_name() {
-        return first_name;
-    }
+    public String getLast_name() { return last_name; }
+    public void setLast_name(String last_name) { this.last_name = last_name; }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getStreet_number() {
-        return street_number;
-    }
-
-    public void setStreet_number(String street_number) {
-        this.street_number = street_number;
-    }
-
-    public String getStreet_name() {
-        return street_name;
-    }
-
-    public void setStreet_name(String street_name) {
-        this.street_name = street_name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", first_name='" + first_name + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", street_number='" + street_number + '\'' +
-                ", street_name='" + street_name + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                '}';
-    }
+    public Set<Address> getAddresses() { return addresses; }
+    public void setAddresses(Set<Address> addresses) { this.addresses = addresses; }
 }
